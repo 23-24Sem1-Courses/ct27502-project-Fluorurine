70449177fe24d8d3976d76ae0de36f9386e8bfaa
Fluorurine <votruong830430073@gmail.com>
2023-08-28 12:32:00 +0700

    Hoan thien PDO, 404, Header và Footer

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Load Jquerry and Tailwind -->
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
+  <title>Web bán quần áo</title>
+</head>
+
+<body>
+
 
   <?php
   require 'vendor/autoload.php';
 
+
   $request = $_SERVER['REQUEST_URI'];
   $path = parse_url($request, PHP_URL_PATH);
   $filename = basename($path, ".php");
-  echo $filename . "\r\n";
-  // echo __DIR__;
+  // echo $filename . "\r\n";
+  // Render phần header tuỳ vào điều kiện đăng nhập
+  if (1 == 1) {
+    require '.\\view\\header.php';
+  }
   switch ($filename) {
     case '':
       require  ".\\view\\home.php";
@@ -21,4 +39,9 @@
       require ".\\view\\404.php";
       break;
   }
-  echo "PHP is running" ?>
+  if (1 == 1) {
+    require '.\\view\\footer.php';
+  }
+  ?></body>
+
+</html>
